203985aa9a25a29fa27385c7a346b3526d625830
Franco Ortega <francopdx@gmail.com>
2021-02-02 15:31:13 -0800

    add Router, Switch, and Routes to App.jsx; add initial Home and Details files

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,11 +1,32 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+} from 'react-router-dom';
 import AllCatFacts from '../cats/AllCatFacts';
+import Details from '../details/Details';
+import Home from '../home/Home';
 
 export default function App() {
   return (
     <>
-      <h1>Hello Cats!</h1>
-      <AllCatFacts />
+      <Router>
+        <h1>Hello Cats!</h1>
+        <AllCatFacts />
+        <Switch>
+          <Route
+            path="/"
+            exact
+            render={(routerProps) => <Home {...routerProps} />}
+          />
+          <Route
+            path="/details/:catFactDetails"
+            exact
+            render={(routerProps) => <Details {...routerProps} />}
+          />
+        </Switch>
+      </Router>
     </>
   );
 }
